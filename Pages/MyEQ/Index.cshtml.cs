57f9dae6ea7923d76214326d394baffeef697bef
Dominik Kitsun <Kitsun.Gaming.Konta@gmail.com>
2023-05-31 16:35:43 +0200

    Initial Commit

---
+++ Pages/MyEQ/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hephaestus_Project.Pages.MyEQ
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
